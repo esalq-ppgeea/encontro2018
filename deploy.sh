5b1ad2b64ba11924df7edd2d05b18380332c34df
Eduardo Junior <edujrrib@gmail.com>
2018-11-26 13:15:42 -0200

    Copy pdf files to gh-pages

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,18 @@ git config --global user.email "edujrrib@gmail.com"
 git config --global user.name "Travis boot to build VIII Encontro website"
 
 git clone -b gh-pages https://${GIT_KEY}@github.com/${TRAVIS_REPO_SLUG}.git dir-tmp
+
 cd dir-tmp
 rm -r ./*
 cp -r ../site/public/* ./
+
+mkdir slides
+cp -r ../slides/* ./slides/
+
+cp ../annals-book/annals.pdf ./
+
+cp ../certifcados/certificate*.pdf ./
+
 git add --all *
 git commit -m "Atualização automática (travis build ${TRAVIS_BUILD_NUMBER})" || true
 git push origin gh-pages
